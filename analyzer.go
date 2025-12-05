c3b8ece1e23979c37582fae5b42e7980016788c3
codestutis <kestutisbiskis@gmail.com>
2025-12-05 03:34:31 -0500

    asdf

--- analyzer.go
+++ analyzer.go
@@ -2,10 +2,17 @@
 package gameanalyzer
 
 import (
+	"log"
+
 	"github.com/Chesstutis/GameAnalyzer/internal/config"
+	"github.com/Chesstutis/GameAnalyzer/internal/engine"
+	"github.com/Chesstutis/GameAnalyzer/internal/logic"
+	"github.com/Chesstutis/GameAnalyzer/internal/parser"
+	"github.com/Chesstutis/GameAnalyzer/internal/types"
 )
 
 type Analyzer struct {
+
 }
 
 // creates a new game analyzer
@@ -17,15 +24,39 @@ func NewAnalyzer() (*Analyzer, error) {
 	return &Analyzer{}, nil
 }
 
-
-
 // analyzes the game pgn using internal/parser/pgn.go and returns a AnalysisResult
-func (a *Analyzer) AnalyzeGame(pgnString string) (*AnalysisResult, error) {
-	var allPuzzles []Puzzle
-	var allMoveEvals []MoveStat
+func (a *Analyzer) AnalyzeGame(pgnString string) (*types.AnalysisResult, error) {
+	var allPuzzles []types.Puzzle
+	var allMoveEvals []types.MoveStat // used for game review
+
+	parsedMoves, err := parser.ParsePGN(pgnString)
+
+	if err != nil {
+		log.Fatalf("failed to parse PGN %v", err)
+	}
+
+	for i, m := range parsedMoves {
+		analysis, err := engine.AnalyzePosition(m.FEN, m.Move)
+		if err != nil {
+			log.Fatalf("error analyzing position %v", err)
+		}
+
+		stat := logic.ProcessMove(analysis.BestMove, analysis.PlayerMove, i % 2 == 0)
+
+		allMoveEvals = append(allMoveEvals, *stat)
+
+		if logic.IsPuzzleWorthy(stat.Classification) {
+			allPuzzles = append(allPuzzles, types.Puzzle{
+				FEN: m.FEN,
+				BestMove: analysis.BestMove.Move,
+				PlayerMove: m.Move,
+				Classification: stat.Classification,
+			})
+		}
+	}
 
 
-	result := &AnalysisResult{
+	result := &types.AnalysisResult{
 		Accuracy: 0,
 		Puzzles: allPuzzles,
 		MoveStats: allMoveEvals,
--- analyzer_test.go
+++ analyzer_test.go
@@ -1,1 +1,26 @@
 package gameanalyzer
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+const (
+	scholarsPgn = "1. e4 e5 2. Bc4 Nc6 3. Qh5 Nf6 4. Qxf7# 1-0"
+)
+
+func TestAnalyzeGame_ScholarsMate(t *testing.T) {
+	a, err := NewAnalyzer()
+	if err != nil {
+		log.Fatalf("problem creating analyzer %v", err)
+	}
+	gameResult, err := a.AnalyzeGame(scholarsPgn)
+
+	if err != nil {
+		log.Fatalf("problem analyzing game %v", err)
+	}
+
+	fmt.Printf("Puzzles: %v\n\n", gameResult.Puzzles)
+	fmt.Printf("MoveStats: %v\n\n", gameResult.MoveStats)
+	
+}
